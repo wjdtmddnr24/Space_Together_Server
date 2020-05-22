46ed0ccdb866cdc8e826b5c8f4c62bb5a180fe21
Hia <kancho1216@naver.com>
2020-05-22 19:34:55 +0900

    friend 라우팅 설정

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
+const friendRouter = require('./friendRouter');
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/friend', friendRouter);
 
 module.exports = router;
